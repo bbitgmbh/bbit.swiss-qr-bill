a14e1bc5684d996a705b16e271086ace1c2b41bd
Yannik Eicher <ache@bbit.io>
2019-11-30 21:12:13 +0100

    feat(bill): started implementation of pdf generation

--- src/bill.ts
+++ src/bill.ts
@@ -1,1 +1,31 @@
-export class BillSlipGenerator {}
+import { QRCodeGenerator } from './qr';
+import { CustomWritableStream } from './stream';
+import { isNodeJs } from './utils';
+import { IQRBill } from './../dist/types/interfaces.d';
+import PDFDocument from 'pdfkit';
+
+export class QRBillGenerator {
+  private _qr = new QRCodeGenerator();
+  public async generate(params: IQRBill): Promise<Buffer | Blob> {
+    const doc = new PDFDocument();
+    const stream = new CustomWritableStream();
+    const code = await this._qr.generate(params);
+    doc.pipe(stream);
+
+    doc.fontSize(25).text('Swiss QR bill', 100, 100);
+
+    doc.image(code, 200, 200, { width: 100 });
+
+    doc.end();
+
+    return new Promise((resolve): void => {
+      stream.on('finish', (): void => {
+        if (isNodeJs) {
+          resolve(stream.toBuffer());
+        } else {
+          resolve(stream.toBlob());
+        }
+      });
+    });
+  }
+}
