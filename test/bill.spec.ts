8854ab6dec60fa861ee0b4339eb37f91086f2afe
Yannik Eicher <ache@bbit.io>
2019-12-02 17:49:30 +0100

    test(bill): added image snapshot tests

--- test/bill.spec.ts
+++ test/bill.spec.ts
@@ -1,12 +1,27 @@
+import { pdfBufferToImage } from './utils';
 import { QRBillGenerator } from './../src/bill';
 import { defaultData } from './data';
+import { toMatchImageSnapshot } from 'jest-image-snapshot';
+expect.extend({ toMatchImageSnapshot });
 
 const bill = new QRBillGenerator();
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace jest {
+    // eslint-disable-next-line @typescript-eslint/interface-name-prefix
+    interface Matchers<R, T> {
+      toMatchImageSnapshot(): R;
+    }
+  }
+}
 
 describe('QRBill', (): void => {
   it('should create bills', async (): Promise<void> => {
     const data = await bill.generate(defaultData);
     expect(data).toBeDefined();
-    // expect(data).toMatchSnapshot();
+    const image = await pdfBufferToImage(data);
+    // TODO test output with https://www.npmjs.com/package/jest-image-snapshot
+    expect(image).toMatchImageSnapshot();
   });
 });
---
+++ test/utils.ts
@@ -0,0 +1,49 @@
+import * as PDFJS from 'pdfjs-dist';
+import { createCanvas } from 'canvas';
+import * as fs from 'fs';
+
+class NodeCanvasFactory {
+  public create(width, height): any {
+    // assert(width > 0 && height > 0, 'Invalid canvas size');
+    const canvas = createCanvas(width, height);
+    const context = canvas.getContext('2d');
+    return {
+      canvas,
+      context,
+    };
+  }
+
+  public reset(canvasAndContext, width, height): void {
+    // assert(canvasAndContext.canvas, 'Canvas is not specified');
+    // assert(width > 0 && height > 0, 'Invalid canvas size');
+    canvasAndContext.canvas.width = width;
+    canvasAndContext.canvas.height = height;
+  }
+
+  public destroy(canvasAndContext): void {
+    // assert(canvasAndContext.canvas, 'Canvas is not specified');
+
+    // Zeroing the width and height cause Firefox to release graphics
+    // resources immediately, which can greatly reduce memory consumption.
+    canvasAndContext.canvas.width = 0;
+    canvasAndContext.canvas.height = 0;
+    canvasAndContext.canvas = null;
+    canvasAndContext.context = null;
+  }
+}
+
+export const pdfBufferToImage = async (pdfBuffer: Buffer | ArrayBuffer): Promise<Buffer> => {
+  const pdf = await PDFJS.getDocument({ data: pdfBuffer, disableFontFace: false });
+  console.log(pdf);
+  const page1 = await pdf.getPage(1);
+  const viewport = page1.getViewport({ scale: 1.5 });
+  const canvasFactory = new NodeCanvasFactory();
+  const canvasAndContext = canvasFactory.create(viewport.width, viewport.height);
+  const renderContext = {
+    canvasContext: canvasAndContext.context,
+    viewport: viewport,
+    canvasFactory: canvasFactory,
+  };
+  await page1.render(renderContext).promise;
+  return Promise.resolve(canvasAndContext.canvas.toBuffer());
+};
